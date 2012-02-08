2fe758e201cc08034d431d8e4006e23362cff3d1
Steve Campbell <dukeytoo@gmail.com>
2012-02-08 16:26:29 -0600

    Basic working proxy, more testing required

--- bin/proxy.js
+++ bin/proxy.js
@@ -4,6 +4,7 @@ var perfectapi = require('perfectapi');
 //var perfectapi = require('../../perfectapi/api.js')
 var path = require('path');
 var main = require('../lib/main.js');
+var load = require('../lib/load.js');
 
 var configPath = path.resolve(__dirname, '..', 'perfectapi.json');
 var parser = new perfectapi.Parser();
@@ -11,26 +12,35 @@ var pollInterval = 60 * 1000;
 
 //handle the commands
 parser.on("getLoad", function(config, callback) {
-  main.getLoad(config, function(err, result) {
+  load.getLoad(config, function(err, result) {
     callback(err, result);
   });
 });
 
 parser.on("getCulprit", function(config, callback) {
-  main.getCulprit(db, config, function(err, result) {
+  load.getCulprit(config, function(err, result) {
     callback(err, result);
   });
 });
 
-module.exports = parser.parse(configPath);
-
-perfectapi.proxy('http://localhost/paas/registry', function(err, registry) {
-  //now we have a reference to the registry running on the same machine
+parser.on("server", function(config, callback) {
+  var port = config.environment.SERVICE_REGISTRY_PORT;
+  var endpoint = 'http://localhost:' + port + '/paas/registry';
+  //endpoint = 'http://services.perfectapi.com/paas/registry';    //for testing
   
-  setInterval(function() {
-    var config = {}
-    registry.listServices(config, function(err, result) {
-      //todo: stuff
-    })
-  }, pollInterval);
-});
+  perfectapi.proxy(endpoint, function(err, registry) {
+    //now we have a reference to the registry running on the same machine
+    setInterval(function() {
+      var config = {}
+      registry.listServices(config, function(err, result) {
+        if (err) return console.log(err);
+        
+        main.poll(registry, result, function(err) {
+          if (err) return console.log(err);
+        });
+      })
+    }, pollInterval);
+  });
+})
+
+module.exports = parser.parse(configPath);
